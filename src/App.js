54e583ddc93d9141228c645073ea028cc8805c18
rainbow <rainbowtech.rnd@gmail.com>
2023-08-07 15:09:21 +0900

    three.js react without three-fiber test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import ThreeScene from './ThreeScene';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        return (
+      <div className="App">
+        {/* Other components and content */}
+        <ThreeScene />
+      </div>
   );
 }
-
 export default App;
---
+++ src/ThreeScene.js
@@ -0,0 +1,63 @@
+import React, { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const ThreeScene = () => {
+    const sceneRef = useRef(null);
+    let scene, camera, renderer, mesh, controls;
+
+    useEffect(() => {
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+        camera.position.z = 250;
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setClearColor(0xccccff);
+        sceneRef.current.appendChild(renderer.domElement);
+
+        // const light = new THREE.AmbientLight(0xffffff);
+        // scene.add(light);
+
+        const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444, 1.0);
+        hemisphereLight.position.set(0, 1, 0);
+        scene.add(hemisphereLight);
+
+        const directionalLight = new THREE.DirectionalLight(0xffeedd);
+        directionalLight.position.set(0, 1, 0).normalize();
+        scene.add(directionalLight);
+
+        const mtlLoader = new MTLLoader();
+        mtlLoader.load('Model.mtl', function (materials) {
+            materials.preload();
+            const objLoader = new OBJLoader();
+            objLoader.setMaterials(materials);
+            objLoader.load('Model.obj', function (object) {
+                mesh = object;
+                scene.add(mesh);
+            });
+        });
+
+        controls = new OrbitControls(camera, renderer.domElement);
+
+        const animate = () => {
+            renderer.render(scene, camera);
+            requestAnimationFrame(animate);
+        };
+
+        animate();
+
+        // Cleanup code
+        return () => {
+            scene.remove(mesh);
+            controls.dispose();
+            renderer.dispose();
+        };
+    }, []);
+
+    return <div ref={sceneRef}></div>;
+};
+
+export default ThreeScene;
